9af3661eb22e171fbfa14a9065cbd80c4907e2d7
Obi Wan <john@thejohnpage.com>
2022-10-11 16:16:31 +0000

    Changed team name

--- src/app.config.js
+++ src/app.config.js
@@ -4,7 +4,7 @@
  * DO NOT store state here - must be part of a stateless service
  */
 class AppConfig {
-    team = 'Your Team Name';
+    team = 'Obi Wan';
     version = '1.0.0';
 }
 module.exports = AppConfig;
